880cf6c00b3f85ed4d06e6fb8024ed712d8ccab0
kesfet <kesfet@zohomail.in>
2026-04-14 12:08:42 +0000

    Legacy Unstable Parser

--- deprecated/parser.c
+++ deprecated/parser.c
@@ -2,6 +2,170 @@
 *	Legacy:		unstable compression logic! 
 *
 */
+#define TOKEN_FILE "parsed_token_file.txt"
+
+#include "includes/lexer.h"
+#include "includes/parser.h"
+#include "includes/token.h"
+#define MAX 100
+
+
+
+/*
+ *	Token *lexer(const char *buffer, size_t len, int *main_counter)
+ *
+ *
+ */
+Token  *parser(const char *buffer, size_t len , size_t *res_count){
+	assert(len > 0);
+	int main_counter = 0;
+	Token	*tokens = lexer(buffer, len, &main_counter);
+	for (int i = 0; i < main_counter; i++) {
+	    printf("Index: %d | Type: %d | Value: %d | Jump: %d\n",
+	           i,
+	           tokens[i].type,
+	           tokens[i].value,
+	           tokens[i].jump);
+	}
+
+	//Array Of Instructions
+	
+	Token   *shorted_tokens		= malloc(MAX * sizeof(Token));
+	int	 shorted_tokens_ptr	= 0;
+	//Shorted Token Store: token type & value : INC_MEM_PTR 10
+	
+	//JUMP LOGIC
+	int 	cur_blk_loc[MAX];
+	int 	blk_loc_pos 		= -1;
+
+	int 	tok_size    		=  0;
+	int 	has_token		=  0;	
+	int 	k 			=  0;
+	Token	tok[MAX];
+	
+
+	//LOOP
+		for(int i = 0; i < main_counter; i++){
+			//(*res_count)++;
+			TokenType T = tokens[i].type;
+			if(T == INC_MEM_PTR || T == DEC_MEM_PTR || T == INC_BLK_VAL || T == DEC_BLK_VAL ){	
+				tok[tok_size].type 	= T; 
+				tok[tok_size].value 	=  1;
+				tok[tok_size].jump  	= -1;
+				tok_size++;	
+			}
+	//---JUMP---	
+			//tokens[i].jump = -1;
+			if(T == CUR_BLK_OPEN){
+				cur_blk_loc[++blk_loc_pos] = shorted_tokens_ptr;
+				shorted_tokens[shorted_tokens_ptr].type 	= tokens[i].type;
+				shorted_tokens[shorted_tokens_ptr].value 	=  1;
+				shorted_tokens[shorted_tokens_ptr].jump		= -2; //[	
+				shorted_tokens_ptr++;
+				has_token++;
+				continue;
+			}
+			if(T == CUR_BLK_CLOSE){
+				if(blk_loc_pos < 0){
+					fprintf(stderr, "Parser error. \n");
+					return NULL;
+				}
+
+    				int open_idx = cur_blk_loc[blk_loc_pos--];
+    				int close_idx = shorted_tokens_ptr;
+
+    				shorted_tokens[shorted_tokens_ptr].type  = T;
+    				shorted_tokens[shorted_tokens_ptr].value = 1;
+    				shorted_tokens[shorted_tokens_ptr].jump  = open_idx;
+    				shorted_tokens[open_idx].jump 		 = close_idx;
+    				shorted_tokens_ptr++;
+				continue;
+			}
+	//---
+	
+	//---COMPRESSION---
+		
+			if(has_token == 0){
+				//tok[0].value++;
+				shorted_tokens[shorted_tokens_ptr].type 	= tok[0].type;
+				shorted_tokens[shorted_tokens_ptr].value 	= tok[0].value;
+				shorted_tokens[shorted_tokens_ptr].jump		= tok[0].jump;	
+				shorted_tokens_ptr++;
+				has_token++;
+			}
+			
+			if( k > 0 && tok_size > 1 
+					&& (T == INC_MEM_PTR || T == DEC_MEM_PTR || T == INC_BLK_VAL || T == DEC_BLK_VAL )){
+				/*
+				 *	When there is 2 tokens in stack 
+				 *	they are equal , increase first value by one 
+				 *	set pointer back to current pointer minus one 
+				 */
+ 
+				if(tok[tok_size - 2].type == tok[tok_size - 1].type){
+					//Same Token In Series Inc The Value 
+						
+						tok[tok_size - 2].value++;
+						tok_size--;
+				}
+
+				/*
+				 *	When new element is not simmiler to the previous one
+				 *	save the old cmd & its value in structured tokens memory 
+				 *	replace older one with new one
+				 *
+				 */
+				if( tok_size > 1 && tok[tok_size - 2].type != tok[tok_size - 1].type ){
+					//store older one into structured tokens memory
+					//if memory is not modified yet , value will be equal to 0
+					//increase it by one and push it into stack 
+					if(tok[tok_size - 2].value == -1){
+						tok[tok_size - 2].value++;
+
+					}
+					//tok[tok_size - 2].value++;
+					shorted_tokens[shorted_tokens_ptr].type 	= tok[tok_size - 2].type;
+					shorted_tokens[shorted_tokens_ptr].value 	= tok[tok_size - 2].value;
+					shorted_tokens[shorted_tokens_ptr].jump		= tok[tok_size - 2].jump;
+					
+					shorted_tokens_ptr++;
+					tok[tok_size - 2].type  = tok[tok_size - 1].type;
+					tok[tok_size - 2].value = tok[tok_size - 1].value;
+					tok[tok_size - 2].jump  = tok[tok_size - 1].jump;
+					tok_size--;	
+				}
+				tok_size++;
+			}
+			
+				
+			k++;
+	//---
+		
+			
+		}
+	(*res_count)	=(size_t)shorted_tokens_ptr;
+	//LOOP END
+
+
+
+	if(blk_loc_pos > -1){
+		fprintf(stderr, "Parser error. \n");	
+		return NULL;
+	}
+	
+	free(tokens);
+
+	return shorted_tokens;
+}
+
+
+
+
+
+//OLDER VERISON 
+
+/*
+
 #include "lexer.h"
 #include "parser.h"
 #include "token.h"
@@ -111,3 +275,4 @@ void parser(const char *buffer, size_t len){
 }
 
 
+*/
